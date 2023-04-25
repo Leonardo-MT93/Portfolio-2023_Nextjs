abf47c771ca63573af4b9c6a62711070721b0e7d
Leonardo Tolaba <leonardotolaba.20@gmail.com>
2023-04-25 06:48:07 -0300

    First commit

---
+++ src/pages/contact.js
@@ -0,0 +1,17 @@
+import TransitionEffect from '@/components/TransitionEffect'
+import Head from 'next/head'
+import React from 'react'
+
+const contact = () => {
+  return (
+    <>
+    <Head>
+        <title>LeoTolaba | Contact Page</title>
+        <meta name="description" content="any description" />
+      </Head>
+      <TransitionEffect/>
+    </>
+  )
+}
+
+export default contact
